40576122fd7f8f23994ad459a37e7864667e9790
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-05 20:06:07 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroIntroducido = Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+
+        int factorial = 1;
+
+        for (int i = 1; i <= numeroIntroducido; i++) {
+            factorial = 1;
+            for (int j = 1; j <= i; j++) {
+                factorial *= j;
+            }
+            System.out.printf("%d! = %d%n", i, factorial);
+        }
     }
 }
